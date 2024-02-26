b03ac1a5f746cb9dd69544342d46d8e4254b8059
李开广 <likaiguang@songguo7.com>
2024-02-26 14:58:29 +0800

    更改quick-start

--- kg-ctl-quickstart/src/main/java/org/lkg/MyJob.java
+++ kg-ctl-quickstart/src/main/java/org/lkg/MyJob.java
@@ -1,35 +1,35 @@
 package org.lkg;
 
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.xxl.job.core.handler.annotation.XxlJob;
-import org.aspectj.weaver.ast.Or;
+import org.kg.ctl.core.DataSyncCommonProcessor;
 import org.kg.ctl.mapper.DbBatchQueryMapper;
-import org.kg.ctl.strategy.impl.TableIndexRangeProcessor;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Description: 如何基于脚手架快速开发属于自己数据同步
  * 1. 配置多数据源，因为至少从A到B有两个
- * 2. 引入依赖，从三个粒度【按业务id同步、按时间同步、按分表同步】选取
- * 3. 配置任务频次，见配置文件
- * 4. 拉取认为
+ * 2. 引入依赖，更具需要继承
+ * <ul>
+ *     <li>{@link  org.kg.ctl.core.DataSyncCommonProcessor} : 基于时间段数据同步</li>
+ *     <li>{@link  org.kg.ctl.core.DataCheckProcessor} : 数据比对</li>
+*      <li>{@link  org.kg.ctl.core.UniqueKeyDataSyncProcessor} : 基于唯一索引数据同步</li>
+ *
+ * </ul>
+ * 3. 配合job 参数，默认基于xxl-job
+ * 4. 配置频次控制参数，可基于apollo、nacos配中心等
  * Author: 李开广
  * Date: 2023/10/17 6:58 PM
  */
 @Component
-public class MyJob extends TableIndexRangeProcessor<Order> {
+public class MyJob extends DataSyncCommonProcessor<Order, Order> {
 
 
-    public MyJob(DbBatchQueryMapper<Order> dbBatchQueryMapper) {
-        super(dbBatchQueryMapper);
+    public MyJob(DbBatchQueryMapper<Order> from, DbBatchQueryMapper<Order> target, IService<Order> iService) {
+        super(from, target, iService);
     }
 
     @XxlJob("doBatchSyncOrder")
@@ -37,18 +37,18 @@ public class MyJob extends TableIndexRangeProcessor<Order> {
         super.runTask();
     }
 
+    @Override
+    protected List<Order> convertToTargetObject(Collection<Order> sourceData, String tableName) {
+        return null;
+    }
 
     @Override
-    protected void batchToTarget(Collection<Order> sourceData) {
-        // 写入你的目标数据库逻辑，比如
-        List<?> list = new ArrayList<>();
-        for (Order sourceDatum : sourceData) {
-            // 完成你的目的数据类型的转换
-            // list.add(convert(sourceDatum))
-        }
-        // syncToTidb();
-        // syncToES();
-        // 注意你无需开发多线程，因为改操作已经内置，sourceData的数据取决你的配置大小，你只需要考虑怎么同步到你的目标服务上，
-        // 例如tidbService.bathInsert(sourceData);
+    public boolean idIncrement() {
+        return false;
+    }
+
+    @Override
+    public String uniqueKey() {
+        return null;
     }
 }
